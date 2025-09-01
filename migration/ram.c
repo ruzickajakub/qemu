bce4217fbb17d655af1568dad052066fde294459
Jakub Růžička <jakub.ruzicka@matfyz.cz>
2025-09-01 23:14:32 +0000

    migration: Outgoing migration for confidential guests
    Functions to manage the RAM migration of confidential guests - start and
    stop the migration handler inside the SVSM and page packaging.
    
    Current shortcomings:
    - The address send cannot be read by SVSM (may be problem on SVSM side)

--- migration/ram.c
+++ migration/ram.c
@@ -36,6 +36,7 @@
 #include "ram.h"
 #include "migration.h"
 #include "migration-stats.h"
+#include "migration/snp.h"
 #include "migration/register.h"
 #include "migration/misc.h"
 #include "qemu-file.h"
@@ -63,6 +64,8 @@
 #include "sysemu/kvm.h"
 
 #include "hw/boards.h" /* for machine_dump_guest_core() */
+#include "hw/i386/x86.h"
+
 
 #if defined(__linux__)
 #include "qemu/userfaultfd.h"
@@ -1251,7 +1254,17 @@ static int ram_save_page(RAMState *rs, PageSearchStatus *pss)
     ram_addr_t offset = ((ram_addr_t)pss->page) << TARGET_PAGE_BITS;
     ram_addr_t current_addr = block->offset + offset;
 
-    p = block->host + offset;
+
+    if (x86_is_machine_confidential()) {
+        uint8_t buf[TARGET_PAGE_SIZE] = {0};
+        snp_package_page(current_addr);
+
+        MigrationState *s = migrate_get_current();
+        cpu_physical_memory_read(s->svsm_migration_page, buf, TARGET_PAGE_SIZE);
+        p = buf;
+    } else {
+        p = block->host + offset;
+    }
     trace_ram_save_page(block->idstr, (uint64_t)offset, p);
 
     XBZRLE_cache_lock();
@@ -2384,6 +2397,9 @@ static void ram_save_cleanup(void *opaque)
         }
     }
 
+    if(x86_is_machine_confidential()) {
+        snp_stop_migration_handler();
+    }
     ram_bitmaps_destroy();
 
     xbzrle_cleanup();
@@ -3057,6 +3073,10 @@ static int ram_save_setup(QEMUFile *f, void *opaque, Error **errp)
     }
 
     migration_ops = g_malloc0(sizeof(MigrationOps));
+
+    if (x86_is_machine_confidential()) {
+        snp_start_migration_handler();
+    }
 
     if (migrate_multifd()) {
         multifd_ram_save_setup();
